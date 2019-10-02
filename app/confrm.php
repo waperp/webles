f41266dd317d6088f82b38ed5ba0f9839866e9e1
Miguel Angel <miguelwisse@wisse.tech>
2019-10-02 15:41:11 -0400

    up

--- app/confrm.php
+++ app/confrm.php
@@ -10,24 +10,24 @@ class confrm extends Model
 
     public function parent() {
 
-        return $this->hasOne('App\confrm', 'confrmscode', 'confrmsfcod')->orderBy('confrmyorde','asc');
+        return $this->hasOne('App\confrm', 'confrmscode', 'confrmsfcod')->where('confrmbenbl',1)->orderBy('confrmyorde','asc');
 
     }
 
     public function children() {
 
-        return $this->hasMany('App\confrm', 'confrmsfcod', 'confrmscode')->orderBy('confrmyorde','asc');
+        return $this->hasMany('App\confrm', 'confrmsfcod', 'confrmscode')->where('confrmbenbl',1)->orderBy('confrmyorde','asc');
 
     }  
 
     public static function tree() {
 
-        return static::with(implode('.', array_fill(0, 4, 'children')))->where('confrmsfcod', '=', NULL)->orderBy('confrmyorde','asc')->get();
+        return static::with(implode('.', array_fill(0, 4, 'children')))->where('confrmbenbl',1)->where('confrmsfcod', '=', NULL)->where('confrmyadmf',0)->orderBy('confrmyorde','asc')->get();
 
     }
     public static function treeAdmin() {
 
-        return static::with(implode('.', array_fill(0, 4, 'children')))->where('confrmsfcod', '=', NULL)->orderBy('confrmyorde','asc')->get();
+        return static::with(implode('.', array_fill(0, 4, 'children')))->where('confrmbenbl',1)->where('confrmsfcod', '=', NULL)->orderBy('confrmyorde','asc')->get();
 
     }
 }
