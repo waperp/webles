0fa573494afa3193ec8687a0949b4080a59ff075
Miguel Angel <miguelwisse@wisse.tech>
2019-10-09 14:38:08 -0400

    up

--- resources/views/layouts/menu-admin.blade.php
+++ resources/views/layouts/menu-admin.blade.php
@@ -6,13 +6,13 @@
         <ul class="elementskit-dropdown elementskit-submenu-panel">
             @foreach($item['children'] as $child)
             @if ($child['children']->count() <= 0) 
-            <li><a href="#menu-{{Str::slug($child->confrmttitl) }}"> <i class="{{ $child->confrmvsmai }}"></i>{{ $child->confrmttitl }}</a>
+            <li><a href="#menu-{{Str::slug($child->confrmttitl) }}"> <i class="{{ $child->confrmvsmai }}"></i> {{ $child->confrmttitl }}</a>
     </li>
     @else
-    <li class="elementskit-dropdown-has"><a href="#"><i class="{{ $child->confrmvsmai }}"></i>{{ $child->confrmttitl }}</a>
+    <li class="elementskit-dropdown-has"><a href="#"><i class="{{ $child->confrmvsmai }}"></i> {{ $child->confrmttitl }}</a>
         <ul class="elementskit-dropdown elementskit-submenu-panelelementskit-dropdown">
             @foreach($child['children'] as $child2)
-            <li><a href="#menu-{{ Str::slug($child2->confrmttitl) }}"><i class="{{ $child2->confrmvsmai }}"></i>{{ $child2->confrmttitl }}</a></li>
+            <li><a href="#menu-{{ Str::slug($child2->confrmttitl) }}"><i class="{{ $child2->confrmvsmai }}"></i> {{ $child2->confrmttitl }}</a></li>
             @endforeach
         </ul>
     </li>
--- resources/views/layouts/menu-client.blade.php
+++ resources/views/layouts/menu-client.blade.php
@@ -12,7 +12,7 @@
                     @if ($child['children']->count() <=  0)
                     <li><a href="#menu-{{ Str::slug($child->confrmttitl) }}"><i class="{{ $child->confrmvsmai }}"></i> {{ $child->confrmttitl }}</a></li>
                     @else
-                        <li class="elementskit-dropdown-has"><a href="#"><i class="{{ $child->confrmvsmai }}"></i>{{ $child->confrmttitl }}</a>
+                        <li class="elementskit-dropdown-has"><a href="#"><i class="{{ $child->confrmvsmai }}"></i> {{ $child->confrmttitl }}</a>
                             @if($child->confrmyadmf == 0)
                             <ul class="elementskit-dropdown elementskit-submenu-panel">
                             @foreach($child['children'] as $child2)
