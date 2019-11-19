5bb1e396b83a865d00cdd4f7588309fe6ac3927d
Miguel Angel <miguelwisse@wisse.tech>
2019-11-19 16:19:05 -0400

    up

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -5,8 +5,10 @@
 @include('layouts.header')
 <section class="text-center p-5">
         <div id="fb-root"></div>
-        <div class="fb-share-button" data-href="https://ademonline.com/noticia" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fademonline.com%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Compartir</a></div>
-        
+        <div class="fb-share-button" 
+        data-href="https://www.ademonline.com/noticia" 
+        data-layout="button_count">
+      </div>
 </section>
 
 @endsection
